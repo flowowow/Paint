cae9bc5e6f0e6eed207e8f9b9afdb137d8581578
jakob Schlachter <jakob.schlachte@gmail.com>
2021-04-19 10:58:18 +0200

    Ellipsen Tool eingebaut

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Paint
+﻿using System;
+using System.Windows.Forms;
+
+namespace Paint
 {
     partial class Form1
     {
@@ -38,6 +41,17 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+
+            this.MouseDown += MousePressed;
+            this.MouseUp += MouseReleased;
+            this.MouseMove += MouseMoved;
+
+            this.EllipsisButton = new Button();
+            EllipsisButton.Text = "Press for Ellipsis Tool";
+            EllipsisButton.Size = new System.Drawing.Size(70,60);
+            EllipsisButton.Location = new System.Drawing.Point(40,10);
+            EllipsisButton.Click += new EventHandler(EnableEllipsis);
+            this.Controls.Add(EllipsisButton);
             
         }
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,8 @@ namespace Paint
     public partial class Form1 : Form
     {
         int pen_size = 10;
-        bool mouse_active = false;
+        Button EllipsisButton;
+        bool mouse_active, EToolActive = false;
         Point old_point = new Point();
         Point new_point = new Point();
         public Form1()
@@ -16,9 +17,7 @@ namespace Paint
             InitializeComponent();
 
             // Assigning mouse events
-            this.MouseDown += MousePressed;
-            this.MouseUp += MouseReleased;
-            this.MouseMove += MouseMoved;
+            
         }
 
         protected void MousePressed(object instance, MouseEventArgs args)
@@ -33,16 +32,37 @@ namespace Paint
 
         protected void MouseReleased(object instance, MouseEventArgs args)
         {
-            if (args.Button == MouseButtons.Left)
+            if (args.Button == MouseButtons.Left && !EToolActive)
             {
                 old_point = new Point();
                 mouse_active = false;
+            }
+            else if(args.Button == MouseButtons.Left && EToolActive)
+            {
+                Graphics graphics = CreateGraphics();
+                new_point = new Point(args.X, args.Y);
+                graphics.DrawEllipse(Pens.Brown,old_point.X, old_point.Y, new_point.X - old_point.X, new_point.Y - old_point.Y);
+                //Console.WriteLine("oldx: {0}, oldy: {1}, newx: {2},newy: {3}",old_point.X, old_point.Y, new_point.X, new_point.Y);
+                old_point = new_point;
+                mouse_active = false;
+            }
+        }
+
+        private void EnableEllipsis(object instance, EventArgs e)
+        {
+            if(EToolActive == true){
+                EToolActive = false;
+                EllipsisButton.Text = "Press for Ellipsis Tool";
+            }
+            else if (EToolActive == false){
+                EToolActive = true;
+                EllipsisButton.Text = "Ellipsis Tool is active";
             }
         }
 
         protected void MouseMoved(object instance, MouseEventArgs args)
         {
-            if (mouse_active)
+            if (mouse_active && !EToolActive)
             {
                 Graphics graphics = CreateGraphics();
                 new_point = new Point(args.X, args.Y);
